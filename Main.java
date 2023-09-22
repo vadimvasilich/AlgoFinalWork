936d9b227df9ca9af3547ff8eec99a66c22dff2b
Vadim Panchenko <121247491+vadimvasilich@users.noreply.github.com>
2023-09-22 22:35:07 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BinaryRedBlackTree<Integer> tree = new BinaryRedBlackTree<>();
+
+        tree.add(7);
+        tree.add(4);
+        tree.add(9);
+        tree.add(56);
+        tree.add(32);
+        tree.add(5);
+        tree.add(8);
+        tree.add(67);
+        tree.add(1);
+        tree.add(3);
+        tree.add(90);
+        tree.add(45);
+        tree.add(12);
+
+        tree.print();
+
+    }
+}
